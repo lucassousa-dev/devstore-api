9c5bea44394af4889fff9e6fdc0ce58a7f06d95e
lucassousa-dev <lucas.player994@gmail.com>
2026-06-06 16:21:14 -0300

    refactor: Substitui lista em memória por Entity Framework

---
+++ DevStore.Api/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DevStore.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevStore.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
